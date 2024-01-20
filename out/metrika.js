a1ee0fab2c9d92660e378293abbeaab402004e07
MixelTe <63655075+MixelTe@users.noreply.github.com>
2024-01-20 09:15:06 +0000

    Deploying to gh-pages from @ MixelTe/learner@f0277d23bad8ed37624562d087404967e21863aa 🚀

--- out/metrika.js
+++ out/metrika.js
@@ -2,22 +2,30 @@ import { DayStatistics } from "./dayStatistics.js";
 import { Keys } from "./keys.js";
 const code = 96188813;
 export function metrika_pageSwitch(prevPage, page, title) {
-    ym(code, "hit", "#" + page, {
+    saveCall(() => ym(code, "hit", "#" + page, {
         params: {
             title,
             referer: "#" + prevPage,
         }
-    });
+    }));
 }
 export function metrika_event(event) {
-    ym(code, "reachGoal", event);
+    saveCall(() => ym(code, "reachGoal", event));
 }
 export function metrika_setParams() {
     const theme = localStorage.getItem(Keys.theme) || "auto";
     const dark = theme != "auto" ? theme == "dark" : window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-    ym(code, "userParams", {
+    saveCall(() => ym(code, "userParams", {
         darkTheme: dark,
         defaultTheme: theme == "auto",
         longestDays: DayStatistics.getLongest(),
-    });
+    }));
+}
+function saveCall(f) {
+    try {
+        f();
+    }
+    catch (e) {
+        console.error(e);
+    }
 }
