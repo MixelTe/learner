d5e7948f9a82aa8238954072cce4deaa6984d66f
Mike <63655075+MixelTe@users.noreply.github.com>
2024-01-24 21:54:49 +0300

    ru_14

--- ts/data/sections.ts
+++ ts/data/sections.ts
@@ -5,6 +5,7 @@ import { ru_10 } from "./ru_10.js"
 import { ru_11 } from "./ru_11.js"
 import { ru_12 } from "./ru_12.js"
 import { ru_13 } from "./ru_13.js"
+import { ru_14 } from "./ru_14.js"
 import { ru_3 } from "./ru_3.js"
 import { ru_4 } from "./ru_4.js"
 import { ru_5 } from "./ru_5.js"
@@ -30,7 +31,7 @@ export const Sections: Section[] = [
 			{ id: "ru_11", name: "№11 Правописание суффиксов", color: themes.blue, items: ru_11 },
 			{ id: "ru_12", name: "№12 Пра­во­пи­са­ние глаголов", color: themes.blue, items: ru_12 },
 			{ id: "ru_13", name: "№13 Правописание НЕ", color: themes.blue, items: ru_13 },
-			// { id: "ru_14", name: "№14 Слитное, дефисное, раздельное написание слов", color: themes.blue, items: ru_14 },
+			{ id: "ru_14", name: "№14 Слитно, дефис или раздельно", color: themes.blue, items: ru_14 },
 			// { id: "ru_15", name: "№15 Н и НН в суффиксах", color: themes.blue, items: ru_15 },
 			// { id: "ru_16", name: "№16-21 Пунктуация", color: themes.blue, items: ru_16 },
 			// { id: "ru_26", name: "№26 Cредства выразительности", color: themes.blue, items: ru_21 },
